4996e0c8467166d2609a26959d328f5589715cec
OjasKarmarkar <ojask2002@gmail.com>
2019-06-11 20:03:22 +0530

    Added Welcome text

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,13 +8,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 220,
+            child: Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 130.0, left: 40),
+                      child: Text("User,",
+                          style: TextStyle(
+                              fontFamily: 'sans-serif',
+                              fontSize: 45,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0, left: 40),
+                      child: Text("Welcome",
+                          style: TextStyle(
+                              letterSpacing: 1.2,
+                              fontFamily: 'sans-serif',
+                              fontSize: 30,
+                              fontWeight: FontWeight.w500)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
