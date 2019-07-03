0cd28bd27236dd008ce19a4b66d3fc609b324a89
OjasKarmarkar <ojask2002@gmail.com>
2019-07-03 19:30:19 +0530

    bookmarks

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,3 +1,4 @@
+import 'package:beats/screens/Bookmarks.dart';
 import 'package:flutter/material.dart';
 import 'package:expanding_bottom_bar/expanding_bottom_bar.dart';
 import 'package:provider/provider.dart';
@@ -14,24 +15,29 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   var index = 0;
-  var screens = [HomeScreen(), Library(), SettingsScreen()];
+  var screens = [HomeScreen(), Library(), Bookmarks(), SettingsScreen()];
   ThemeData _themeData;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
           body: screens[index],
           bottomNavigationBar: ExpandingBottomBar(
+            animationDuration: Duration(milliseconds: 700),
             backgroundColor: Theme.of(context).backgroundColor,
               navBarHeight: 75,
               items: [
                 ExpandingBottomBarItem(
-                    icon: Icons.bookmark_border,
+                    icon: Icons.home,
                     text: "Home",
                     selectedColor: Colors.pinkAccent),
                 ExpandingBottomBarItem(
                     icon: Icons.music_note,
                     text: "Library",
                     selectedColor: Colors.red),
+                    ExpandingBottomBarItem(
+                    icon: Icons.bookmark_border,
+                    text: "Bookmarks",
+                    selectedColor: Colors.deepPurpleAccent),
                 ExpandingBottomBarItem(
                     icon: Icons.settings,
                     text: "Settings",
