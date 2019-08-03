760aabc765cac28bd07c72b960681e10dc9e106e
OjasKarmarkar <ojask2002@gmail.com>
2019-08-03 19:09:07 +0530

    test

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -214,11 +214,17 @@ class _HomeScreenState extends State<HomeScreen> {
                                       child: Padding(
                                         padding: const EdgeInsets.all(8.0),
                                         child: IconButton(
-                                          icon: Icon(Icons.delete_outline , color: Colors.white,),
-                                          onPressed: () async{
-                                            PlaylistHelper temp = await PlaylistHelper(playlistRepo.playlist[pos]);
+                                          icon: Icon(
+                                            Icons.delete_outline,
+                                            color: Colors.white,
+                                          ),
+                                          onPressed: () async {
+                                            PlaylistHelper temp =
+                                                await PlaylistHelper(
+                                                    playlistRepo.playlist[pos]);
                                             temp.deletePlaylist();
-                                            playlistRepo.delete(playlistRepo.playlist[pos]);
+                                            playlistRepo.delete(
+                                                playlistRepo.playlist[pos]);
                                             //playlistRepo.init();
                                           },
                                         ),
@@ -227,15 +233,154 @@ class _HomeScreenState extends State<HomeScreen> {
                                     Align(
                                       alignment: Alignment.topRight,
                                       child: Padding(
-                                        padding: const EdgeInsets.only(right: 30.0 , top: 8.0),
+                                        padding: const EdgeInsets.only(
+                                            right: 40.0, top: 8.0),
                                         child: IconButton(
-                                          icon: Icon(Icons.edit, color: Colors.white,),
+                                          icon: Icon(
+                                            Icons.edit,
+                                            color: Colors.white,
+                                          ),
                                           onPressed: () {
-                                            setState(() {
-                                              playlistRepo.playlist[pos] = "Fuck";
-                                            });
-                                            
-
+                                            showDialog(
+                                              context: context,
+                                              builder: (context) {
+                                                return Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(8.0),
+                                                  child: AlertDialog(
+                                                    backgroundColor:
+                                                        Theme.of(context)
+                                                            .backgroundColor,
+                                                    shape:
+                                                        RoundedRectangleBorder(
+                                                      side: BorderSide(),
+                                                      borderRadius:
+                                                          BorderRadius.all(
+                                                              Radius.circular(
+                                                                  30.0)),
+                                                    ),
+                                                    contentPadding:
+                                                        EdgeInsets.only(
+                                                            top: 10.0),
+                                                    content: Container(
+                                                      width: 200.0,
+                                                      child: Column(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .start,
+                                                        crossAxisAlignment:
+                                                            CrossAxisAlignment
+                                                                .stretch,
+                                                        mainAxisSize:
+                                                            MainAxisSize.min,
+                                                        children: <Widget>[
+                                                          Row(
+                                                            mainAxisAlignment:
+                                                                MainAxisAlignment
+                                                                    .spaceEvenly,
+                                                            mainAxisSize:
+                                                                MainAxisSize
+                                                                    .min,
+                                                            children: <Widget>[
+                                                              Text(
+                                                                "Edit",
+                                                                style: TextStyle(
+                                                                    fontSize:
+                                                                        24.0,
+                                                                    fontFamily:
+                                                                        'Sans'),
+                                                              ),
+                                                            ],
+                                                          ),
+                                                          SizedBox(
+                                                            height: 5.0,
+                                                          ),
+                                                          Divider(
+                                                            color: Colors.grey,
+                                                            height: 4.0,
+                                                          ),
+                                                          Padding(
+                                                            padding:
+                                                                EdgeInsets.only(
+                                                                    left: 30.0,
+                                                                    right: 30.0,
+                                                                    top: 30.0,
+                                                                    bottom:
+                                                                        30.0),
+                                                            child:
+                                                                TextFormField(
+                                                              controller: txt,
+                                                              decoration: InputDecoration(
+                                                                  disabledBorder: OutlineInputBorder(
+                                                                      borderSide: BorderSide(
+                                                                          color: Colors
+                                                                              .greenAccent)),
+                                                                  enabledBorder: OutlineInputBorder(
+                                                                      borderSide: BorderSide(
+                                                                          color: Colors
+                                                                              .greenAccent)),
+                                                                  errorText: error
+                                                                      ? "Name cant be null"
+                                                                      : null,
+                                                                  errorStyle: Theme.of(
+                                                                          context)
+                                                                      .textTheme
+                                                                      .display2,
+                                                                  labelText:
+                                                                      "Enter Name",
+                                                                  labelStyle: Theme.of(
+                                                                          context)
+                                                                      .textTheme
+                                                                      .display2,
+                                                                  border: OutlineInputBorder(
+                                                                      borderRadius:
+                                                                          BorderRadius.circular(
+                                                                              4))),
+                                                            ),
+                                                          ),
+                                                          InkWell(
+                                                            onTap: () {
+                                                              playlistRepo.playlist[pos] = "HI";
+                                                              Navigator.pop(context);
+                                                            },
+                                                            child: Container(
+                                                              padding: EdgeInsets
+                                                                  .only(
+                                                                      top: 10.0,
+                                                                      bottom:
+                                                                          20.0),
+                                                              decoration:
+                                                                  BoxDecoration(
+                                                                color:
+                                                                    Colors.blue,
+                                                                borderRadius: BorderRadius.only(
+                                                                    bottomLeft:
+                                                                        Radius.circular(
+                                                                            32.0),
+                                                                    bottomRight:
+                                                                        Radius.circular(
+                                                                            32.0)),
+                                                              ),
+                                                              child: Text(
+                                                                "Done",
+                                                                style: TextStyle(
+                                                                    fontFamily:
+                                                                        'Sans',
+                                                                    color: Colors
+                                                                        .white),
+                                                                textAlign:
+                                                                    TextAlign
+                                                                        .center,
+                                                              ),
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ),
+                                                  ),
+                                                );
+                                              },
+                                            );
                                           },
                                         ),
                                       ),
